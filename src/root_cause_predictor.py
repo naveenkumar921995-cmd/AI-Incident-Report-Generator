cd5facbe6fdd33c7a8126f32229872f8ad15655f
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-03-09 16:58:32 +0530

    Update root_cause_predictor.py

--- src/root_cause_predictor.py
+++ src/root_cause_predictor.py
@@ -1,11 +1,16 @@
 import joblib
+import os
 
-model = joblib.load("models/root_cause_model.pkl")
-vectorizer = joblib.load("models/vectorizer.pkl")
+# Get project root directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+model_path = os.path.join(BASE_DIR, "models", "root_cause_model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "models", "vectorizer.pkl")
+
+model = joblib.load(model_path)
+vectorizer = joblib.load(vectorizer_path)
 
 def predict_root_cause(description):
-
     X = vectorizer.transform([description])
     prediction = model.predict(X)
-
     return prediction[0]
