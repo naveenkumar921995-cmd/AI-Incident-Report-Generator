9f64e97f400794848c55b0bc995e2d731571d58b
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-03-09 17:01:10 +0530

    Update root_cause_predictor.py

--- src/root_cause_predictor.py
+++ src/root_cause_predictor.py
@@ -1,17 +1,29 @@
-import joblib
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+data_path = os.path.join(BASE_DIR, "data", "incidents_dataset.csv")
+
+# Load dataset
+data = pd.read_csv(data_path)
+
+X = data["incident"]
+y = data["root_cause"]
+
+# Train model
+vectorizer = CountVectorizer()
+X_vec = vectorizer.fit_transform(X)
+
+model = RandomForestClassifier()
+model.fit(X_vec, y)
+
+
 def predict_root_cause(description):
 
-    model_path = os.path.join(BASE_DIR, "models", "root_cause_model.pkl")
-    vectorizer_path = os.path.join(BASE_DIR, "models", "vectorizer.pkl")
-
-    model = joblib.load(model_path)
-    vectorizer = joblib.load(vectorizer_path)
-
-    X = vectorizer.transform([description])
-    prediction = model.predict(X)
+    X_input = vectorizer.transform([description])
+    prediction = model.predict(X_input)
 
     return prediction[0]
