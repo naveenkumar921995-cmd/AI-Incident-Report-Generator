9f5e0b6f17b7331aee349d1b55d8fe6c23632359
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-03-09 17:03:58 +0530

    Update root_cause_predictor.py

--- src/root_cause_predictor.py
+++ src/root_cause_predictor.py
@@ -7,19 +7,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 data_path = os.path.join(BASE_DIR, "data", "incidents_dataset.csv")
 
-# Load dataset
 data = pd.read_csv(data_path)
 
 X = data["incident"]
 y = data["root_cause"]
 
-# Train model
 vectorizer = CountVectorizer()
 X_vec = vectorizer.fit_transform(X)
 
 model = RandomForestClassifier()
 model.fit(X_vec, y)
-
 
 def predict_root_cause(description):
 
