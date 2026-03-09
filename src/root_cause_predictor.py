dbdb7cb1c410a3030dabec7211ddf329a135ee3e
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-03-09 17:19:12 +0530

    Update root_cause_predictor.py

--- src/root_cause_predictor.py
+++ src/root_cause_predictor.py
@@ -1,31 +1,25 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
-import os
+from pathlib import Path
 
-# Get project root directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# project root
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+data_path = BASE_DIR / "data" / "incidents_dataset.csv"
+
+data = pd.read_csv(data_path)
+
+X = data["incident"]
+y = data["root_cause"]
+
+vectorizer = CountVectorizer()
+X_vec = vectorizer.fit_transform(X)
+
+model = RandomForestClassifier()
+model.fit(X_vec, y)
 
 def predict_root_cause(description):
-
-    # dataset path
-    data_path = os.path.join(BASE_DIR, "data", "incidents_dataset.csv")
-
-    # load dataset
-    data = pd.read_csv(data_path)
-
-    X = data["incident"]
-    y = data["root_cause"]
-
-    # train model
-    vectorizer = CountVectorizer()
-    X_vec = vectorizer.fit_transform(X)
-
-    model = RandomForestClassifier()
-    model.fit(X_vec, y)
-
-    # prediction
     X_input = vectorizer.transform([description])
     prediction = model.predict(X_input)
-
     return prediction[0]
