38cf337ca4711b77f1915ad2520ef2c03411cdee
naveenkumar921995-cmd <naveenkumar921995@gmail.com>
2026-03-13 07:08:25 +0530

    Update root_cause_predictor.py

--- src/root_cause_predictor.py
+++ src/root_cause_predictor.py
@@ -1,11 +1,9 @@
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from pathlib import Path
 
-# project root
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 data_path = BASE_DIR / "data" / "incidents_dataset.csv"
 
 data = pd.read_csv(data_path)
@@ -13,13 +11,15 @@ data = pd.read_csv(data_path)
 X = data["incident"]
 y = data["root_cause"]
 
-vectorizer = CountVectorizer()
+vectorizer = TfidfVectorizer()
 X_vec = vectorizer.fit_transform(X)
 
 model = RandomForestClassifier()
 model.fit(X_vec, y)
 
 def predict_root_cause(description):
+
     X_input = vectorizer.transform([description])
     prediction = model.predict(X_input)
+
     return prediction[0]
